44dcacbfcf258ca1f1908e1b98a20c6c84ce59cd
pu1et <pu1etproof@gmail.com>
2021-10-05 18:19:26 +0900

    feat : 게임 재시작 및 종료 메시지 출력 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,9 +4,15 @@ import nextstep.utils.Console;
 
 public class InputView {
     private static final String INPUT_MSG = "숫자를 입력해주세요 : ";
+    private static final String NEW_GAME_OR_QUIT_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static String getUserInput() {
         System.out.printf("%s", INPUT_MSG);
         return Console.readLine();
+    }
+
+    public static int getNewGameOrQuitGameInput() {
+        System.out.println(NEW_GAME_OR_QUIT_MSG);
+        return Integer.parseInt(Console.readLine());
     }
 }
