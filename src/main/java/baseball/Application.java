ff79e52fb6e46efb33b82b9dd880d011047ad748
pu1et <pu1etproof@gmail.com>
2021-10-05 18:33:23 +0900

    feat : 게임 플레이어 입력 로직 및 BaseballGame 인스턴스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,9 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
 import baseball.domain.TargetGenerator;
+import baseball.domain.UserInputValidator;
+import baseball.view.InputView;
 
 public class Application {
 
@@ -11,10 +14,25 @@ public class Application {
 
     private static void startNewGame() {
         String targetNumber = setTargetNumber();
+        String userNumber = setUserNumber();
+
+        BaseballGame baseBallGame = new BaseballGame(userNumber, targetNumber);
     }
 
     static String setTargetNumber() {
         TargetGenerator targetGenerator = new TargetGenerator();
         return targetGenerator.getTargetNumber();
+    }
+
+    private static String setUserNumber() {
+        String userInput = InputView.getUserInput();
+        UserInputValidator userInputValidator = new UserInputValidator(userInput);
+
+        while (!userInputValidator.getIsValid()) {
+            InputView.printInputErrorMsg();
+            userInput = InputView.getUserInput();
+            userInputValidator = new UserInputValidator(userInput);
+        }
+        return userInput;
     }
 }
