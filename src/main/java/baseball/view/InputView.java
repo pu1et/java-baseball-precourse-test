a9182944caefd3cfee67a25001c566b2b51a642e
pu1et <pu1etproof@gmail.com>
2021-10-05 18:20:26 +0900

    feat : 플레이어 입력 값에 대한 오류 메시지 출력 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,6 +5,7 @@ import nextstep.utils.Console;
 public class InputView {
     private static final String INPUT_MSG = "숫자를 입력해주세요 : ";
     private static final String NEW_GAME_OR_QUIT_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INPUT_ERROR_MSG = "[ERROR] 잘못된 값을 입력하셨습니다.";
 
     public static String getUserInput() {
         System.out.printf("%s", INPUT_MSG);
@@ -14,5 +15,9 @@ public class InputView {
     public static int getNewGameOrQuitGameInput() {
         System.out.println(NEW_GAME_OR_QUIT_MSG);
         return Integer.parseInt(Console.readLine());
+    }
+
+    public static void printInputErrorMsg() {
+        System.out.println(INPUT_ERROR_MSG);
     }
 }
