a248ed0f2918f54dca9aaa3ec74b3cbbdd6c4c19
pu1et <pu1etproof@gmail.com>
2021-10-05 18:27:50 +0900

    feat : 메인 로직 및 게임 시작 시 타겟 문자 생성 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import baseball.domain.TargetGenerator;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        startNewGame();
+    }
+
+    private static void startNewGame() {
+        String targetNumber = setTargetNumber();
+    }
+
+    static String setTargetNumber() {
+        TargetGenerator targetGenerator = new TargetGenerator();
+        return targetGenerator.getTargetNumber();
     }
 }
