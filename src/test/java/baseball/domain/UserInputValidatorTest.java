22a4ee94c876ae0354fa7276c26043aba812f5db
pu1et <pu1etproof@gmail.com>
2021-10-06 18:22:37 +0900

    test : 게임 플레이어의 잘못된 입력 유효성 검사 기능 테스트 구현

--- src/test/java/baseball/domain/UserInputValidatorTest.java
+++ src/test/java/baseball/domain/UserInputValidatorTest.java
@@ -14,4 +14,16 @@ class UserInputValidatorTest {
 
         assertThat(userInputValidator.getIsValid()).isTrue();
     }
+
+    @Test
+    @DisplayName("게임 플레이어가 1부터 9까지가 아니거나, 서로 다른 숫자가 아니거나, 3자리로 입력하지 않으면 유효성이 통과되지 않아야 한다.")
+    void 게임_플레이어의_잘못된_입력() {
+        UserInputValidator userInputValidator1 = new UserInputValidator("012");
+        UserInputValidator userInputValidator2 = new UserInputValidator("133");
+        UserInputValidator userInputValidator3 = new UserInputValidator("12");
+
+        assertThat(userInputValidator1.getIsValid()).isFalse();
+        assertThat(userInputValidator2.getIsValid()).isFalse();
+        assertThat(userInputValidator3.getIsValid()).isFalse();
+    }
 }
